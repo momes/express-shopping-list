2e90879057f56a202856705144aed61c598fc3e0
sarmini1 <spencerarmini@gmail.com>
2021-05-12 16:56:22 -0700

    finished routes

--- itemRoutes.js
+++ itemRoutes.js
@@ -8,16 +8,21 @@ const router = new express.Router();
 const { NotFoundError, BadRequestError } = require("./expressError");
 const middleware = require("./middleware");
 
-/** GET /items: get list of items */
+/** GET /items: get list of items
+ * 
+ * [{"name":"popsicle","price":1.45},
+ * {"name":"cheerios","price":3.4}]
+ * 
+ */
 router.get("/", function (req, res, next) {
-  return res.json(items);
+  return res.json({"items": items});
 });
 
 /** POST /items: accept JSON body, add item, and return it 
  * {"name": "popsicle", "price": 1.45} =>
   {added: {name: "popsicle", price: 1.45}}
 */
-router.post("/", middleware.validateNewItem, function (req, res, next) {
+router.post("/", middleware.validateItem, function (req, res, next) {
     add_item = {
         name: req.body.name,
         price: +req.body.price
@@ -25,6 +30,66 @@ router.post("/", middleware.validateNewItem, function (req, res, next) {
     items.push(add_item)
     return res.json({added: add_item});
   });
+
+/** GET /items: get list of items
+ * returns:
+ * {"name": "popsicle", "price": 1.45}
+ */
+router.get("/:name", function (req, res, next) {
+
+  //use req.params since we aren't passing this info in the query string
+  let desiredItemName = req.params.name;
+  let foundItem = items.find(item => item.name === desiredItemName);
+
+  if (!foundItem){
+    throw new NotFoundError("Item not found");
+  }
+
+  //in the event that we have multiple items with the same name,
+  // we could filter through our items array and get a new array of all
+  // matching elements, which we could then loop through 
+  // and display in our response
+  return res.json(foundItem);
+});
+
+/*
+PATCH /items/:name: accept JSON body, modify item, return it:
+
+{name: "new popsicle", price: 2.45} =>
+  {updated: {name: "new popsicle", price: 2.45}}
+*/
+router.patch('/:name', middleware.validateItem, function (req, res, next){
+
+  let desiredItemName = req.params.name; //popsicle
+  let foundItem = items.find(item => item.name === desiredItemName)
+
+  if (!foundItem){
+    throw new NotFoundError("Item not found");
+  }
+
+  let newItemName = req.body.name;
+  let newItemPrice = req.body.price;
+
+  foundItem.price = +newItemPrice;
+  foundItem.name = newItemName;
+
+  return res.json({"updated": foundItem});
+})
+
+/*
+DELETE /items/:name: delete item:
+
+{message: "Deleted"}
+*/
+router.delete('/:name', function (req, res, next){
+
+  let desiredItemName = req.params.name;
+  let foundItem = items.find(item => item.name === desiredItemName)
+
+  items.splice(items.indexOf(foundItem), 1);
+
+  return res.json({"message": "Deleted"});
+})
   
 
 /** DELETE /users/[id]: delete user, return {message: Deleted} */
--- middleware.js
+++ middleware.js
@@ -1,10 +1,10 @@
 const { NotFoundError, BadRequestError } = require("./expressError");
 
-function validateNewItem(req, res, next) {
+function validateItem(req, res, next) {
     if (!req.body.name || !req.body.price || isNaN(+req.body.price) ) {
         throw new BadRequestError("Must provide name of item and price (as a number)")
     }
     return next();
 }
 
-module.exports = { validateNewItem };
+module.exports = { validateItem };
